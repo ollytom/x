3e5dd7d4d4ee367a339053fa14f5121026408c01
Oliver Lowe <o@olowe.co>
2025-04-06 10:36:06 +1000

    lib/plumbing: import

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ cp bin/* $HOME/bin
 mkdir -p $HOME/.config/git
 cp lib/git $HOME/.config/git/config
 
+mkdir -p $HOME/lib
+cp lib/plumbing $HOME/lib
+
 if uname | grep OpenBSD
 then
 	mkdir -p $HOME/.config/gtk-3.0 $HOME/.config/gtk-4.0
